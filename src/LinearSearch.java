83a7a01484e7a79bedcb287dcfb1d185fa3b4dab
Hasitha.Kaluarachchi <kaluarachchigehasitha@gmail.com>
2025-11-16 08:22:38 +0530

    Update LinearSearch.java

--- src/LinearSearch.java
+++ src/LinearSearch.java
@@ -1,5 +1,28 @@
 package AlgorithmAnalyzer;
 
+import java.util.Random;
+
 public class LinearSearch {
+
+    // Linear Search Algorithm
+    public static int linearSearch(int[] arr, int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                return i;  // Element found
+            }
+        }
+        return -1;  // Element not found
+    }
+
+    // Generate random array
+    public static int[] generateArray(int size) {
+        Random r = new Random();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = r.nextInt(10000);
+        }
+        return arr;
+    }
+
     
 }
